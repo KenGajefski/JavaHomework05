376130c0887f1787c36834a612c3eb0719fc8619
Kenneth Gajefski <kenneth.ag@outlook.com>
2018-04-01 22:05:55 -0400

    Both constructors added to Product class. Could be wrong according to directions. Getting clarification.

--- src/HW5.java
+++ src/HW5.java
@@ -3,6 +3,7 @@ public class HW5 {
     public static void main(String[] args){
 
 
+
     }
 
 }
--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,25 @@
 public class Product {
+
+    private static int productCount;
+    private static double inventoryValue;
+    private static int inventoryCount;
+    private int code;
+    private String name;
+    private double cost;
+    private int count;
+
+    public Product(){
+        productCount = productCount + 1;
+        code = -1;
+        name = "(not set)";
+        cost = -1;
+        count = -1;
+    }
+
+    public Product(int productCount, double inventoryValue, int inventoryCount, int code){
+        this.productCount = productCount + 1;
+        this.inventoryValue = inventoryValue + (cost * count);
+        this.inventoryCount = inventoryCount + count;
+        this.code = code;
+    }
 }
