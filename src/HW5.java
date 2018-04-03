75550339324216cea7c197927fe7054e22f141ef
Kenneth Gajefski <kenneth.ag@outlook.com>
2018-04-03 10:42:10 -0400

    File now able to be read. Working on implementing menu still.

--- src/HW5.java
+++ src/HW5.java
@@ -1,4 +1,6 @@
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class HW5{
@@ -10,16 +12,36 @@ public class HW5{
 
         // Variables
         Scanner fileIn = null;
-        int input;
         Scanner keyboard = new Scanner(System.in);
-        fileIn = new Scanner(new FileInputStream("ProductInventoryIn.txt"));
+        int input;
 
+
+        // Attempt to open input file
+        try
+        {
+            // Assign external file to file handle
+            fileIn = new Scanner(new FileInputStream(FILE_NAME));
+        }
+        // Handle file error
+        catch (FileNotFoundException e)
+        {
+            System.out.println("Error: file '" + FILE_NAME+
+                    "' not found.");
+            System.out.println("Default folder: " +
+                    System.getProperty("user.dir"));
+            System.out.println("Error message:" + e.getMessage());
+        }
+
+
+        // Menu
+        /*
         System.out.println("Make your selection");
         input = keyboard.nextInt();
         while (!(input == 4)){
             // Any other option not on the menu
         }
-
+        */
+        // End of menu
     }
 
 }
