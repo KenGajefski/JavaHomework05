d672391f3a94d4eafb6d3bc4f8b2b5a2b2bc0de1
Kenneth Gajefski <kenneth.ag@outlook.com>
2018-04-03 01:12:30 -0400

    File should be able to be read, but getting an error.

--- src/HW5.java
+++ src/HW5.java
@@ -1,13 +1,18 @@
+import java.io.FileInputStream;
 import java.util.Scanner;
 
 public class HW5{
 
-    public static final String FILE_NAME = "ProductInventoryIn.txt";
+    // Constants and Globals
+    private static final String FILE_NAME = "ProductInventoryIn.txt";
 
     public static void main(String[] args){
 
+        // Variables
+        Scanner fileIn = null;
         int input;
         Scanner keyboard = new Scanner(System.in);
+        fileIn = new Scanner(new FileInputStream("ProductInventoryIn.txt"));
 
         System.out.println("Make your selection");
         input = keyboard.nextInt();
